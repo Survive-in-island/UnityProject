da9a76c0bd45bcbcda932cf562c8df4168e821a4
KimDongSoon <eutteumiyo@naver.com>
2020-04-23 22:56:29 +0900

    rabbitSpawn

--- SurInIsland/Assets/Scripts/PigSpawnManager.cs
+++ SurInIsland/Assets/Scripts/PigSpawnManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PigSpawnManager : MonoBehaviour
 {
-    [Header("Pig Create Info")]
     // 야생동물이 출현할 위치를 담을 배열
     public Transform[] points;
     // 야생동물 프리팹을 저장할 변수
@@ -12,37 +11,16 @@ public class PigSpawnManager : MonoBehaviour
     // 야생동물을 생성할 주기
     public float animalCreatTime = 5.0f;
     // 야생동물 최대 생성 개수
-    public int maxAnimal = 10;
+    public int maxAnimal = 5;
 
     public bool isGameOver = false;
-
-    // 싱글턴에 접근하기 위한 static 변수 선언
-    public static PigSpawnManager instance = null;
-
-    //[Header("Object Pool")]
-  
-    void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        // instance에 할당된 클래스의 인스턴스가 다를 경우 새로 생성된 클래스를 의미함
-        else if (instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-
-        // 다른 신으로 넘어가도 삭제하지 않고 유지
-        DontDestroyOnLoad(this.gameObject);
-    }
 
     // Start is called before the first frame update
     void Start()
     {
         points = GameObject.Find("PigSpawnPointGroup").GetComponentsInChildren<Transform>();
 
-        if(points.Length > 0)
+        if (points.Length > 0)
         {
             StartCoroutine(this.CreateAnimal());
         }
@@ -75,6 +53,6 @@ public class PigSpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
