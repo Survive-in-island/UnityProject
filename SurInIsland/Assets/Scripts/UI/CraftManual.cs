646070626deba4a0c254bbb47999735523854661
KimDongSoon <eutteumiyo@naver.com>
2020-05-27 16:24:37 +0900

    Craft

--- SurInIsland/Assets/Scripts/UI/CraftManual.cs
+++ SurInIsland/Assets/Scripts/UI/CraftManual.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Craft
+{
+    public string craftName;            // 이름
+    public GameObject go_Prefab;        // 실제 설치될 프리팹
+    public GameObject go_PreviewPrefab; // 미리보기 프리팹
+}
 public class CraftManual : MonoBehaviour
 {
     // 상태변수 
@@ -9,6 +16,18 @@ public class CraftManual : MonoBehaviour
 
     [SerializeField]
     private GameObject go_BaseUI; // 기본 베이스 UI
+
+    [SerializeField]
+    private Craft[] craft_fire; // 모닥불용 탭
+
+    private GameObject go_Preview; // 미리보기 프리팹을 담을 변수 
+
+    private Transform tf_Player;
+
+    public void SlotClick(int _slotNumber)
+    {
+        go_Preview = Instantiate(craft_fire[_slotNumber].go_PreviewPrefab, tf_Player.position + tf_Player.forward, Quaternion.identity);
+    }
 
     // Update is called once per frame
     void Update()
